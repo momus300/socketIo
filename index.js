423bdeab8bda7ce590397f5de76046951ab7d0d6
momus <momus@PC-Komputer.home>
2016-10-18 04:38:16 +0200

    komunikator societIo

---
+++ index.js
@@ -0,0 +1,18 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+        io.emit('chatWiadomosc', 'Hi all! :)');
+    socket.on('chatWiadomosc', function(msg){
+        io.emit('chatWiadomosc', msg);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
